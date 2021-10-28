36543b0b5f9ca3b7855ea57a3426504dca14be48
Dmitriy Strebkov <87489519+DEMOGORGANN@users.noreply.github.com>
2021-10-28 19:31:03 +0300

    finish half home page

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -10,13 +10,13 @@ import ImgInfoServ from './../IMG/ImgInfoServ.png'
 
 import styles from './HomePage.module.css'
 
-function HomePage() {
+function HomePage({ kolUser }) {
 
 
     return (
 
         <div className={styles.WrapHomePage}>
-            <h2>Начни увлекательное выживание на наших серверах!</h2>
+            <h2 className={styles.TITLETILE}>Начни увлекательное выживание на наших серверах!</h2>
             <button>
                 <img src={pickMinecraft} alt="" />
                 <span>НАЧАТЬ ИГРАТЬ</span>
@@ -30,16 +30,54 @@ function HomePage() {
                 </div>
             </div>
             <span className={styles.titleMe}>О НАС</span>
+
             <div className={styles.wrapInfoMe1}>
                 <div className={styles.intoServers}>
                     <h2>На серверах ежедневно играет более 10000 пользователей!</h2>
-                    <div></div>
+
+                    <div className={styles.wrapUserInfoServ}>
+
+                        <div className={styles.UserInfoServ}>
+
+
+                            <div className={styles.cirleFull}>
+
+                                <div className={`${styles.circle1} ${styles.green}`}>
+                                    <div className={styles.circle2}></div>
+                                </div>
+
+                                <h3 className={styles.greenCol}>
+                                    {kolUser}
+                                </h3>
+
+                            </div>
+
+                            <span>В данный момент на серверах играет</span>
+                        </div>
+                        <div className={styles.UserInfoServ}>
+
+                            <div className={styles.cirleFull}>
+
+                                <div className={`${styles.circle1} ${styles.blue}`}>
+                                    <div className={styles.circle2}></div>
+                                </div>
+
+                                <h3 className={styles.blueCol}>
+                                    414244
+                                </h3>
+
+                            </div>
+
+                            <span>Кол-во зарегистрированных игроков</span>
+                        </div>
+
+                    </div>
+
                 </div>
-                <div>
-                    <img src={ImgInfoServ} alt="" />
-                </div>
+
+                <img src={ImgInfoServ} alt="" />
             </div>
-        </div>
+        </div >
 
     );
 }
