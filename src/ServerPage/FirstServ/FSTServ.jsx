245fd25ae501c1c3a1cc96892ea77b906bb3d2a6
Dmitriy Strebkov <87489519+DEMOGORGANN@users.noreply.github.com>
2021-11-02 20:50:59 +0300

    minimal changed SeversPage and refactoring code

---
+++ src/ServerPage/FirstServ/FSTServ.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import styles from './FSTServ.module.css'
+
+
+import PlayAndCopy from "./../PlayAndCopy/PlayAndCopy";
+
+
+import quetionImg from './../../IMG/quetionImg.png'
+
+
+function FSTServ() {
+
+
+
+    return (
+        <div className={styles.FTSTServ}>
+            <div className={styles.wrapEl}>
+                <div className={styles.HeadElem}>
+                    <div className={styles.leftInfo}>
+                        <div className={styles.lobby}>
+                            <span className={styles.forBot}>общее лобби</span>
+                            <div className={styles.tooltip}>
+                                <img src={quetionImg} alt="" />
+                                <div className={styles.tooltiptext}>Общее лобби позволяет игроку подключиться к серверу и с помощью специального портала перемещаться по режимам!</div>
+                            </div>
+                        </div>
+                        <span>pc.hypegame.com</span>
+                    </div>
+                    <PlayAndCopy copy={"pc.hypegame.co"} />
+                </div>
+                <div className={styles.bottomElem}>
+                    <progress max="2500" value="2000"></progress>
+
+                    <div className={styles.infoOnline}>
+                        <div className={styles.circle1}>
+                            <div className={styles.circle2}></div>
+                        </div>
+                        <span>1500/2500</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FSTServ;
