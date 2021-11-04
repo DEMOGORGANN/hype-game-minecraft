134a7fbbd28cc0c92ba382220a953946b00d8e9b
Dmitriy Strebkov <87489519+DEMOGORGANN@users.noreply.github.com>
2021-11-04 23:32:16 +0300

    head setup BanList

--- src/BanList/BanList.jsx
+++ src/BanList/BanList.jsx
@@ -3,9 +3,16 @@ import React, { useEffect, useState } from "react";
 
 import styles from './BanList.module.css'
 
+import seachUserBut from './../IMG/seachUser.png'
+
 function BanList() {
 
     const [banList, setBanLins] = useState([])
+
+    let lastUserBan = "";
+    banList.forEach(i => {
+        lastUserBan = i.nameBan;
+    })
 
 
     useEffect(() => {
@@ -16,14 +23,39 @@ function BanList() {
                     setBanLins(result);
                 }
             )
-
     }, [])
 
 
 
     return (
-        <div>
-            
+        <div className={styles.wrapBanList}>
+            <div className={styles.wrapHeadList}>
+                <div className={styles.Elements}>
+                    <span>всего банов:</span>
+                    <h3>{banList.length * 57}</h3>
+                </div>
+                <div className={styles.Elements}>
+                    <span>кол-во администраторов:</span>
+                    <h3>{banList.length * 13}</h3>
+                </div>
+                <div className={styles.Elements}>
+                    <span>кол-во разбаненных:</span>
+                    <h3>{banList.length * 9}</h3>
+                </div>
+                <div className={styles.Elements}>
+                    <span>всего банов:</span>
+                    <h3>{banList.length}</h3>
+                </div>
+                <div className={styles.Elements}>
+                    <span>последний бан:</span>
+                    <h3>{lastUserBan}</h3>
+                </div>
+                <div className={styles.MyInput}>
+                    <input type="text" placeholder="Поиск игрока" />
+                    <button><img src={seachUserBut} alt="" /></button>
+                </div>
+            </div>
+
         </div>
     );
 }
