5929fc57f93d7daf865e85c7d5b00e3d09d65618
Dmitriy Strebkov <87489519+DEMOGORGANN@users.noreply.github.com>
2021-11-12 19:21:44 +0300

    finish page shop Donate

---
+++ src/ShopDonate/ModalBuy/ModalBuy.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect } from "react";
+import { useState } from "react/cjs/react.development";
+
+
+import styles from './ModalBuy.module.css'
+
+export default function ModalBuy({ visible, setVisible, modalinfo }) {
+
+    useEffect(() => {
+        document.querySelector("body").style.overflow = visible ? "hidden" : "auto";
+    }, [visible])
+
+
+    const [prises, setPrises] = useState(1)
+
+    if (visible === false) {
+        return null;
+
+
+    } else {
+        return (
+            <div className={styles.modal} onClick={() => setVisible(false)}>
+
+                <div className={styles.modalDialog} onClick={e => e.stopPropagation()}>
+                    <div className={styles.full}>
+                        <div className={styles.headInf}>
+                            <span className={styles.headInfo}>{modalinfo.name}</span>
+                            <img src={modalinfo.img} alt="" />
+                        </div>
+                        <div className={styles.wrapInfoDonat}>
+                            <span>- Смена погоды</span>
+                            <span>- Установка времени для себя</span>
+                            <span>- Запустить огненный шар</span>
+                            <span>- Бессмертие</span>
+                            <span>- Починить вещи</span>
+                            <span>- Чистка инвентаря</span>
+                            <span>- Дать себе опыт</span>
+                            <span>- Прыжок</span>
+                            <span>- Пройти сквозь стену</span>
+                            <span>- Набор</span>
+                            <span>- Эндерсундук</span>
+                            <span>- Префикс на сервире [modalinfo.name]</span>
+                            <span>- Возможность иметь 15 приватов на сервере</span>
+                            <span>- Возможность иметь 15 домов на сервере</span>
+                            <span>- Возможность зайти на заполненный сервер</span>
+
+                        </div>
+                        <span className={styles.headInput}>Особенности привилегии</span>
+                        <div className={styles.wrapInputBut}>
+                            <form>
+                                <div className={styles.wrapOneInput}>
+                                    <input type="radio" name="timeDonate" onClick={() => setPrises(1)} />
+                                    <label>14 дней</label>
+                                </div>
+                                <div className={styles.wrapOneInput}>
+                                    <input type="radio" name="timeDonate" onClick={() => setPrises(1.3)} />
+                                    <label>1 месяц</label>
+                                </div>
+                                <div className={styles.wrapOneInput}>
+                                    <input type="radio" name="timeDonate" onClick={() => setPrises(1.5)} />
+                                    <label>3 месяца</label>
+                                </div>
+                                <div className={styles.wrapOneInput}>
+                                    <input type="radio" name="timeDonate" onClick={() => setPrises(1.8)} />
+                                    <label>6 месяцев</label>
+                                </div>
+                                <div className={styles.wrapOneInput}>
+                                    <input type="radio" name="timeDonate" onClick={() => setPrises(2)} />
+                                    <label>Год</label>
+                                </div>
+                                <div className={styles.wrapOneInput}>
+                                    <input type="radio" name="timeDonate" onClick={() => setPrises(2.5)} />
+                                    <label>Навсегда</label>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                    <button>приобрести за  ₽{Math.floor(modalinfo.price * prises)}</button>
+                </div>
+
+            </div>
+        )
+    }
+}
--- src/ShopDonate/ShopDonate.jsx
+++ src/ShopDonate/ShopDonate.jsx
@@ -6,15 +6,98 @@ import styles from './ShopDonate.module.css'
 
 
 import warrior3 from './../IMG/warrior3.png'
+import warrior6 from './../IMG/warrior6.png'
+import warrior5 from './../IMG/warrior5.png'
+import warrior from './../IMG/PersMIn.png'
+import warrior10 from './../IMG/warrior10.png'
+import warrior4 from './../IMG/warrior4.png'
+import warrior11 from './../IMG/warrior11.png'
+import warrior8 from './../IMG/warrior8.png'
+import ModalBuy from "./ModalBuy/ModalBuy";
 
 function ShopDonate() {
 
-
     const [activeBut, setActiveBut] = useState("Survival")
+    const [visible, setVisible] = useState(false)
+    const [modalinfo, setModalInfo] = useState({})
+    const [SurvivalBlock] = useState(
+        [
+            { id: 1, nameDonate: "ВОИН", price: 199, img: warrior3, divStyle: styles.one },
+            { id: 2, nameDonate: "премиум", price: 299, img: warrior6, divStyle: styles.two },
+            { id: 3, nameDonate: "креатив", price: 499, img: warrior5, divStyle: styles.three },
+            { id: 4, nameDonate: "АДМИН", price: 549, img: warrior, divStyle: styles.four },
+            { id: 5, nameDonate: "лорд", price: 750, img: warrior10, divStyle: styles.fife },
+            { id: 6, nameDonate: "ГЛ.АДМИН", price: 1009, img: warrior4, divStyle: styles.six },
+            { id: 7, nameDonate: "создатель", price: 1499, img: warrior11, divStyle: styles.seven },
+            { id: 8, nameDonate: "ОСНОВАТЕЛЬ", price: 3499, img: warrior8, divStyle: styles.eight },
+        ]
+    )
+    const [GriefBlock] = useState(
+        [
+            { id: 1, nameDonate: "ВОИН", price: 199, img: warrior3, divStyle: styles.one },
+            { id: 2, nameDonate: "премиум", price: 299, img: warrior6, divStyle: styles.two },
+            { id: 3, nameDonate: "креатив", price: 499, img: warrior5, divStyle: styles.three },
+            { id: 4, nameDonate: "АДМИН", price: 549, img: warrior, divStyle: styles.four },
+        ]
+    )
+    const [PvPAreaBlock] = useState(
+        [
+            { id: 1, nameDonate: "ВОИН", price: 199, img: warrior3, divStyle: styles.one },
+            { id: 2, nameDonate: "премиум", price: 299, img: warrior6, divStyle: styles.two },
+        ]
+    )
+
+    const BlogDonate = () => {
+
+        let viewDonate = SurvivalBlock;
+
+        if (activeBut === "Survival") {
+            viewDonate = SurvivalBlock;
+        } else if (activeBut === "Grief") {
+            viewDonate = GriefBlock;
+        } else if (activeBut === "PvPArea") {
+            viewDonate = PvPAreaBlock;
+        } else {
+            viewDonate = SurvivalBlock
+        }
+
+        return (
+            <div className={styles.wrapPageDodateBlock}>
+                {
+                    viewDonate.map(i => {
+                        return (
+
+                            <div
+                                className={`${styles.wrapDonateBlock} ${i.divStyle}`}
+                                key={i.id}
+                                onClick={() => {
+                                    setVisible(true);
+                                    setModalInfo({ name: i.nameDonate, price: i.price, img: i.img })
+                                }}>
+
+                                <div className={styles.infoDonate}>
+                                    <span>{i.nameDonate}</span>
+                                    <span>ОТ ₽{i.price}</span>
+                                </div>
+                                <div>
+                                    <img src={i.img} alt="" />
+                                </div>
+                            </div>
+
+                        )
+                    })
+                }
+            </div>
+        )
+
+    }
+
+
 
 
     return (
         <div className={styles.wrapShop}>
+            <ModalBuy visible={visible} setVisible={setVisible} modalinfo={modalinfo} />
             <div className={styles.wrapBut}>
                 <button
                     className={activeBut === "Survival" ? styles.activeBut : 0}
@@ -37,43 +120,8 @@ function ShopDonate() {
                     новый сервер
                 </button>
             </div>
-            <div className={styles.wrapDonateBlock}>
-                <div className={styles.infoDonate}>
-                    <span>ВОИН</span>
-                    <span>ОТ ₽199</span>
-                </div>
-                <div>
-                    <img src={warrior3} alt="" />
-                </div>
-            </div>
-            <div className={styles.wrapDonateBlock}>
-                <div className={styles.infoDonate}>
-                    <span>ВОИН</span>
-                    <span>ОТ ₽199</span>
-                </div>
-                <div>
-                    <img src={warrior3} alt="" />
-                </div>
-            </div>
-            <div className={styles.wrapDonateBlock}>
-                <div className={styles.infoDonate}>
-                    <span>ВОИН</span>
-                    <span>ОТ ₽199</span>
-                </div>
-                <div>
-                    <img src={warrior3} alt="" />
-                </div>
-            </div>
-            <div className={styles.wrapDonateBlock}>
-                <div className={styles.infoDonate}>
-                    <span>ВОИН</span>
-                    <span>ОТ ₽199</span>
-                </div>
-                <div>
-                    <img src={warrior3} alt="" />
-                </div>
-            </div>
-            
+
+            <BlogDonate />
 
         </div>
     );
